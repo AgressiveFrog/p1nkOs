419490f3b389e9b4721fe1ccd3434443bf82021a
Lord Agressive Frog <96744041+AgressiveFrog@users.noreply.github.com>
2023-09-01 09:54:32 +0200

    Update installWizard.lua

--- installWizard.lua
+++ installWizard.lua
@@ -58,69 +58,69 @@ term.write("Opening Install Wizard ...")
 -- INSTALL WIZARD GUI
 
 pos(1, 1)  -- Banner
-colorfy("colors.white", "colors.black", "P1NK.OS INSTALL WIZARD                        ")
+colorfy(1, 32768, "P1NK.OS INSTALL WIZARD                        ")
 pos(46, 1)  -- Exit button
-colorfy(red, "colors.white", "[ X ]")
+colorfy(red, 1, "[ X ]")
 
   -- HARDCODE LOGO
   pos(1,3)
-colorfy("colors.black", "colors.white", "....")  -- LN1
-colorfy ("colors.blue", "colors.white", "#########") 
-colorfy("colors.black", "colors.white", "....")
+colorfy(32768, 1, "....")  -- LN1
+colorfy (2048 , 1, "#########") 
+colorfy(32768, 1, "....")
   pos(1,4)
-colorfy("colors.black", "colors.white", "..")  -- LN2
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.purple", "colors.white", "PP")
-colorfy("colors.pink", "colors.white", "IINNKKPP")
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.black", "colors.white", "..")
+colorfy(32768, 1, "..")  -- LN2
+colorfy(2048 , 1, "##")
+colorfy(4, 1, "PP")
+colorfy(64, 1, "IINNKKPP")
+colorfy(2048 , 1, "##")
+colorfy(32768, 1, "..")
   pos(1,5)
-colorfy("colors.blue", "colors.white", "####")  -- LN3
-colorfy("colors.purple", "colors.white", "II")
-colorfy("colors.pink", "colors.white", "NN")
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.purple", "colors.white", "KK")
-colorfy("colors.pink", "colors.white", "PP")
-colorfy("colors.blue", "colors.white", "####")
+colorfy(2048 , 1, "####")  -- LN3
+colorfy(4, 1, "II")
+colorfy(64, 1, "NN")
+colorfy(2048 , 1, "##")
+colorfy(4, 1, "KK")
+colorfy(64, 1, "PP")
+colorfy(2048 , 1, "####")
   pos(1,6)
-colorfy("colors.blue", "colors.white", "####")  -- LN4
-colorfy("colors.purple", "colors.white", "II")
-colorfy("colors.pink", "colors.white", "NN")
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.purple", "colors.white", "KK")
-colorfy("colors.pink", "colors.white", "PP")
-colorfy("colors.blue", "colors.white", "####")
+colorfy(2048 , 1, "####")  -- LN4
+colorfy(4, 1, "II")
+colorfy(64, 1, "NN")
+colorfy(2048 , 1, "##")
+colorfy(4, 1, "KK")
+colorfy(64, 1, "PP")
+colorfy(2048 , 1, "####")
   pos(1,7)
-colorfy("colors.blue", "colors.white", "####")  -- LN5
-colorfy("colors.purple", "colors.white", "II")
-colorfy("colors.pink", "colors.white", "NN")
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.purple", "colors.white", "KK")
-colorfy("colors.pink", "colors.white", "PP")
-colorfy("colors.blue", "colors.white", "####")
+colorfy(2048 , 1, "####")  -- LN5
+colorfy(4, 1, "II")
+colorfy(64, 1, "NN")
+colorfy(2048 , 1, "##")
+colorfy(4, 1, "KK")
+colorfy(64, 1, "PP")
+colorfy(2048 , 1, "####")
   pos(1,8)
-colorfy("colors.blue", "colors.white", "####")  -- LN6
-colorfy("colors.purple", "colors.white", "II")
-colorfu("colors.pink", "colors.white", "NNKKPPII")
-colorfy("colors.blue", "colors.white", "####")
+colorfy(2048 , 1, "####")  -- LN6
+colorfy(4, 1, "II")
+colorfu(64, 1, "NNKKPPII")
+colorfy(2048 , 1, "####")
   pos(1,9)
-colorfy("colors.black", "colors.white", "########")  -- LN7
-colorfy("colors.purple", "colors.white", "NN")
-colorfy("colors.pink", "colors.white", "KK")
-colorfy("colors.black", "colors.white", "##########")
+colorfy(32768, 1, "########")  -- LN7
+colorfy(4, 1, "NN")
+colorfy(64, 1, "KK")
+colorfy(32768, 1, "##########")
   pos(1,10)
-colorfy("colors.black", "colors.white", "..")  -- LN8 
-colorfy("colors.blue", "colors.white", "##")
-colorfy("colors.purple", "colors.white", "PP")
-colorfy("colors.pink", "colors.white", "II")
-colorfy("colors.blue", "colors.white", "##########")
-colorfy("colors.black", "colors.white", "..")
+colorfy(32768, 1, "..")  -- LN8 
+colorfy(2048 , 1, "##")
+colorfy(4, 1, "PP")
+colorfy(64, 1, "II")
+colorfy(2048 , 1, "##########")
+colorfy(32768, 1, "..")
   pos(1,11)
-colorfy("colors.black", "colors.white", "....")  -- LN9
-colorfy("colors.purple", "colors.white", "NN")
-colorfy("colors.pink", "colors.white", "KK")
-colorfy("colors.blue", "colors.white", "#######")
-colorfy("colors.black", "colors.white", "..os")
+colorfy(32768, 1, "....")  -- LN9
+colorfy(4, 1, "NN")
+colorfy(64, 1, "KK")
+colorfy(2048 , 1, "#######")
+colorfy(32768, 1, "..os")
   -- This should be a 18x9 logo, since each character is 1x2 in size
   -- That fixes the weird stretch, but I am to lazy to change the inside, so I'm keeping the double letters
 
