b473a790953b8e8bac114839fdd91c7011346151
Lord Agressive Frog <96744041+AgressiveFrog@users.noreply.github.com>
2023-09-01 23:59:24 +0200

    Update MailService-client.lua

--- MailService-client.lua
+++ MailService-client.lua
@@ -1,3 +1,9 @@
+-- Fuck this is stupid
+--[[ Just ignore the steaming pile of shit beneath this line
+
+
+
+
 -- Mail Service
 -- CLIENT SIDE
 
@@ -40,3 +46,45 @@ end
 -- Download if needed
 
 -- fszsxf
+--]]
+
+
+term.clear()
+local w, h = term.getSize()
+os.pullEvent = os.PullEventRaw
+
+--https://tweaked.cc/module/parallel.html
+
+-- Print primitive GUI
+-- Get info
+    -- User Email
+    -- ID, label
+    -- Is Connected
+    -- Newest Mails
+-- Make connection
+    -- Scan for server
+    -- If found
+        -- Send Info
+        -- Update Status
+-- Update GUI in case of lag
+    -- Get Status
+    -- Print User Stuff
+    -- Print Menu
+    -- Print mails and shit
+-- Recieve all
+    -- OPEN THE MULTITHRED FLOODGATES!
+-- Ping Server
+    -- Send Pull Request
+
+while true do
+    -- Update GUI, since this is the beginning of a loop
+    -- Ping Server again
+    -- Get Keys
+        -- If writing
+            -- Start Writing Tab
+        -- If read message
+            -- Fullscreen messages
+                -- Scroll
+        -- Exit
+            -- Send termination notice and exit to menu
+        
