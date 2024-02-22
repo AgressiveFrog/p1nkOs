2e9c477dea85892c2470824e9dda41717d67cd45
Lord Agressive Frog <96744041+AgressiveFrog@users.noreply.github.com>
2024-02-22 17:14:26 +0100

    Create P1NK_reworked.lua

---
+++ P1NK_reworked.lua
@@ -0,0 +1,37 @@
+-- Select CD or ISO installation
+-- Format Disks
+-- Select Driver Location
+-- Select Usable Disks
+
+-- Check for updates
+
+-- Validate files
+
+-- Create Sudo User
+
+-- Append Monitors, Modems and Printers
+
+-- Reboot
+
+-- Login as Sudo User
+
+-- 
+
+--[[ 
+    STANDARD .PNK FILE
+    
+    .config    CONTAINS:
+    44 49 53 4B 2E 50 4E 4B 00 00 00 00 00 00 2F 30     DISK.PNK      /0
+    41 55 54 48 2E 4D 41 43 48 49 4E 45 49 44 2F 30     AUTH.MACHINEID/0
+    44 41 54 45 2E 44 44 2D 4D 4D 2D 59 59 00 2F 30     DATE.DD-MM-YY /0
+    45 4E 43 4F 2E 53 54 41 4E 44 41 52 44 00 2F 30     ENCO.STANDARD /0
+    
+    44 49 53 4B 2E 50 4E 4B 00 00 00 00 00 00 2F 30     DISK.PNK_EXEC /0
+    41 55 54 48 2E 4D 41 43 48 49 4E 45 49 44 2F 30     AUTH.PINK_SYS /0
+    44 41 54 45 2E 44 44 2D 4D 4D 2D 59 59 00 2F 30     DATE.25-02-24 /0
+    45 4E 43 4F 2E 53 54 41 4E 44 41 52 44 00 2F 30     ENCO.DEV_OPS  /0
+
+    <DATA>
+
+
+]]
