b72a85a5520b5816b71899dcd9d12e4c2f5533fa
Lord Agressive Frog <96744041+AgressiveFrog@users.noreply.github.com>
2024-02-23 02:43:15 +0100

    Update P1NK_reworked.lua
    worked from home
    added the start of the program

--- P1NK_reworked.lua
+++ P1NK_reworked.lua
@@ -35,3 +35,132 @@
 
 
 ]]
+
+
+-- uhh, so this should be the "startup.lua" file
+-- how the fuck would i overwrite a running program?
+-- ^^ change the name later on the afterboot
+-- ^^ ^^ how?
+-- ^^ ^^ ^^ If _TEMP does not Exist: Create File: _TEMP; reboot as _TEMP; As _TEMP: Delete Startup; Copy Self as Startup; Reboot as Startup; On Startup: Delete _TEMP, Delete termination lines in self code;
+
+--[[P1NK OS
+    CC-TWEAKED
+
+    CREATED BY: DUCK29
+
+
+
+    VER:        1.00.00
+    TYPE:       RELEASE
+
+]]
+
+local version = "1.00.00"
+local release = "PUBLIC"
+
+local function setPos(xValue, yValue)
+    term.setCursorPos(xValue, yValue)
+
+end
+
+local function color(fg, bg)
+    term.setTextColor(fg)
+    term.setBackgroundColor(bg)
+
+end
+
+local function write(value)
+    term.write(value)
+
+end
+
+local function cls(parameter, bgColor)
+    if (parameter == "C") then
+        term.clear()
+
+    elseif (parameter == "B") then
+        term.clear()
+
+        local oldColor = term.getBackgroundColor()
+        term.setBackgroundColor(bgColor)
+
+        for i=1,51,1 do
+            for j=1, 19, 1 do
+                setPos(j, i)
+                write(" ")
+
+            end
+
+        end
+
+        term.setBackgroundColor(oldColor)
+
+    else
+        term.clear()
+
+    end
+
+end
+
+local function menuBanner()
+    local oldFg = term.getTextColor()
+    local oldBg = term.getBackgroundColor()
+
+    color(colors.pink, colors.white)
+    setPos(1, 1)
+    write("P1NK OS")
+    setPos(1, 46)
+    write(version)
+
+    term.setTextColor(oldFg)
+    term.setBackgroundColor(oldBg)
+
+end
+
+-- BEGIN PROGRAM
+
+cls("C")
+setPos(1, 1)
+write("P1NK OS")
+setPos(1, 2)
+write("CREATED BY: DUCK29")
+setPos(1, 6)
+write("VER:")
+setPos(1, 7)
+write("TYPE:")
+
+setPos(13, 6)
+write(version)
+setPos(13, 7)
+write(release)
+
+
+
+sleep(3)
+cls("B", colors.white)
+
+
+local function selectInstallationMethod_static()
+    menuBanner()
+    setPos(13, 6)
+    write("SELECT INSTALLATION METHOD:")
+    setPos(17, 8)
+    write("CD")
+    setPos(37, 8)
+    write("?")
+    setPos(17, 9)
+    write("ISO")
+    setPos(23, 9)
+    write("< RECOMENDED")
+    setPos(37, 9)
+    write("?")
+    setPos(17, 11)
+    write("SELECTED: NONE")
+    setPos(15, 14)
+    write("ABORT")
+    setPos(30, 14)
+    write("CONTINUE")
+
+end
+
+selectInstallationMethod_static()
